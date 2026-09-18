cd10e582eba1e706ad756b3789fd8580d05a4c85
Mridul Mahesh Nambiar <mridulonmail@gmail.com>
2026-09-18 22:26:23 +0530

    First commit

---
+++ CLI_Calculator.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  bool invalid_op=true;
+  double num1,num2,ans;
+  char op;
+  cout << "**********CLI CALCULATOR**********" << '\n';
+  cout << "Enter the first number: ";
+  cin >> num1;
+  while (invalid_op)
+  {
+    cout << "Enter the operation you want to perform (+, -, *, /): ";
+    cin >> op;
+    switch (op)
+      {
+        case '+':
+        cout << "Enter the second number: ";
+        cin >> num2;
+        cout << "Your answer is: " << num1+num2;
+        invalid_op=false;
+        break;
+
+        case '-':
+        cout << "Enter the second number: ";
+        cin >> num2;
+        cout << "Your answer is: " << num1-num2;
+        invalid_op=false;
+        break;
+
+        case '*':
+        cout << "Enter the second number: ";
+        cin >> num2;
+        cout << "Your answer is: " << num1*num2;
+        invalid_op=false;
+        break;
+
+        case '/':
+        cout << "Enter the second number: ";
+        cin >> num2;
+        if (num2==0)
+        {
+          cout << "Division with 0 is not possible! Try again." << '\n';
+          break;
+        }
+        cout << "Your answer is: " << num1/num2;
+        invalid_op=false;
+        break;
+
+        default:
+        cout << "Invalid operator! Try again." << '\n';
+      }
+  }
+  return 0;
+}
