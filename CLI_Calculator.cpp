199e39866ec6029b291b81e846db1bd65d5f2fe8
Mridul Mahesh Nambiar <mridulonmail@gmail.com>
2026-09-19 10:22:28 +0530

    Completed the project but may polish if required

--- CLI_Calculator.cpp
+++ CLI_Calculator.cpp
@@ -2,54 +2,72 @@
 using namespace std;
 int main()
 {
-  bool invalid_op=true;
+  bool invalid_op=true, calc=true;
   double num1,num2,ans;
-  char op;
-  cout << "**********CLI CALCULATOR**********" << '\n';
-  cout << "Enter the first number: ";
-  cin >> num1;
-  while (invalid_op)
+  char op, ct;
+  while (calc)
   {
-    cout << "Enter the operation you want to perform (+, -, *, /): ";
-    cin >> op;
-    switch (op)
-      {
-        case '+':
-        cout << "Enter the second number: ";
-        cin >> num2;
-        cout << "Your answer is: " << num1+num2;
-        invalid_op=false;
-        break;
-
-        case '-':
-        cout << "Enter the second number: ";
-        cin >> num2;
-        cout << "Your answer is: " << num1-num2;
-        invalid_op=false;
-        break;
-
-        case '*':
-        cout << "Enter the second number: ";
-        cin >> num2;
-        cout << "Your answer is: " << num1*num2;
-        invalid_op=false;
-        break;
-
-        case '/':
-        cout << "Enter the second number: ";
-        cin >> num2;
-        if (num2==0)
+    cout << "**********CLI CALCULATOR**********" << '\n';
+    cout << "Enter the first number: ";
+    cin >> num1;
+    while (invalid_op)
+    {
+      cout << "Enter the operation you want to perform (+, -, *, /): ";
+      cin >> op;
+      switch (op)
         {
-          cout << "Division with 0 is not possible! Try again." << '\n';
+          case '+':
+          cout << "Enter the second number: ";
+          cin >> num2;
+          cout << "Your answer is: " << num1+num2;
+          invalid_op=false;
           break;
-        }
-        cout << "Your answer is: " << num1/num2;
-        invalid_op=false;
-        break;
 
-        default:
-        cout << "Invalid operator! Try again." << '\n';
+          case '-':
+          cout << "Enter the second number: ";
+          cin >> num2;
+          cout << "Your answer is: " << num1-num2;
+          invalid_op=false;
+          break;
+
+          case '*':
+          cout << "Enter the second number: ";
+          cin >> num2;
+          cout << "Your answer is: " << num1*num2;
+          invalid_op=false;
+          break;
+
+          case '/':
+          cout << "Enter the second number: ";
+          cin >> num2;
+          if (num2==0)
+          {
+            cout << "Division with 0 is not possible! Try again." << '\n';
+            break;
+          }
+          cout << "Your answer is: " << (double)num1/num2;
+          invalid_op=false;
+          break;
+  
+          default:
+          cout << "Invalid operator! Try again." << '\n';
+        }
+    }
+    invalid_op=true;
+    while (calc)
+    {
+      cout << '\n' << "Continue? (Y/N): ";
+      cin >> ct;
+      if (ct=='Y' || ct=='y')
+        break;
+      else if (ct=='N' || ct=='n')
+        calc=false;
+      else 
+      {
+        cout << "Invalid input! Press Y or N." << '\n';
+        continue;
       }
+    }
   }
   return 0;
 }
